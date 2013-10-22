6b7161f6e299d17403fef9acfd28acddc1dd6264
nathan.scandella <nathan.scandella@gmail.com>
2013-10-22 09:23:12 +0000

    changed script main to use sbsettings daemon scripting via notify_post

--- Script/Script/main.m
+++ Script/Script/main.m
@@ -5,11 +5,13 @@
 
 #import <UIKit/UIKit.h>
 
-#import "AppDelegate.h"
+//#import "AppDelegate.h"
+#include <notify.h>
 
 int main(int argc, char *argv[])
 {
-    int result = system("/var/mobile/script.sh");
-    NSLog(@" result of /var/mobile/script.sh was %d", result);
-    return 0;
+   //int result = system("/var/mobile/script.sh");
+   //NSLog(@" result of /var/mobile/script.sh was %d", result);
+   notify_post("com.mycompany.script");
+   return 0;
 }
